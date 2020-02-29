a3b54d1973c9c863eb734e7335fb6f899c68feb0
Bryan DeGrendel <bryan@degrendel.com>
2020-02-29 16:38:06 -0500

    Add initial Application
    Mostly from the prototype

---
+++ outrogue-frontend/src/main/kotlin/com/degrendel/outrogue/frontend/Application.kt
@@ -0,0 +1,65 @@
+package com.degrendel.outrogue.frontend
+
+import com.degrendel.outrogue.common.logger
+import com.degrendel.outrogue.engine.OutrogueEngine
+import org.hexworks.zircon.api.CP437TilesetResources
+import org.hexworks.zircon.api.ColorThemes
+import org.hexworks.zircon.api.SwingApplications
+import org.hexworks.zircon.api.application.AppConfig
+import org.hexworks.zircon.api.application.CursorStyle
+import org.hexworks.zircon.api.application.DebugConfig
+import org.hexworks.zircon.api.color.TileColor
+import org.hexworks.zircon.api.data.Size
+import org.hexworks.zircon.api.grid.TileGrid
+import java.util.concurrent.locks.Condition
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+
+class Application(lock: ReentrantLock, condition: Condition, profile: LaunchProfile)
+{
+  companion object
+  {
+    val L by logger()
+  }
+
+  val engine = OutrogueEngine()
+
+  val tileGrid: TileGrid
+
+  init
+  {
+    if (profile.soarDebugger)
+      TODO("Open the soar debugger!")
+
+    val debugConfig = if (profile.debugDrawGrid)
+      DebugConfig(displayGrid = true, displayCoordinates = true, displayFps = true)
+    else
+      DebugConfig(displayGrid = false, displayCoordinates = false, displayFps = true)
+
+
+    tileGrid = SwingApplications.startTileGrid(
+        AppConfig(
+            blinkLengthInMilliSeconds = 500,
+            cursorStyle = CursorStyle.FIXED_BACKGROUND,
+            cursorColor = TileColor.defaultForegroundColor(),
+            isCursorBlinking = false,
+            isClipboardAvailable = true,
+            defaultTileset = CP437TilesetResources.rexPaint16x16(),
+            defaultGraphicalTileset = CP437TilesetResources.rexPaint16x16(),
+            defaultColorTheme = ColorThemes.defaultTheme(),
+            title = engine.properties.window.title,
+            fullScreen = profile.fullscreen,
+            debugMode = profile.zirconDebugMode,
+            debugConfig = debugConfig,
+            size = Size.create(engine.properties.window.width, engine.properties.window.height),
+            betaEnabled = false,
+            fpsLimit = engine.properties.window.fpsLimit))
+
+    tileGrid.onShutdown { lock.withLock { condition.signal() } }
+  }
+}
+
+data class LaunchProfile(val fullscreen: Boolean,
+                         val soarDebugger: Boolean,
+                         val zirconDebugMode: Boolean,
+                         val debugDrawGrid: Boolean)
---
+++ outrogue-frontend/src/main/kotlin/com/degrendel/outrogue/frontend/Main.kt
@@ -0,0 +1,47 @@
+package com.degrendel.outrogue.frontend
+
+import com.degrendel.outrogue.common.logger
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import java.util.concurrent.Callable
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+import kotlin.system.exitProcess
+
+@Command(name = "ReverseRogue", mixinStandardHelpOptions = true)
+class Main : Callable<Int>
+{
+  companion object
+  {
+    private val L by logger()
+  }
+
+  private val lock = ReentrantLock()
+  private val condition = lock.newCondition()
+
+  @Option(names = ["--soar-debugger"])
+  private var soarDebugger = false
+
+  @Option(names = ["--zircon-debug-mode"])
+  private var zirconDebugMode = false
+
+  @Option(names = ["--draw-zircon-grid"])
+  private var drawZirconGrid = false
+
+  @Option(names = ["--fullscreen"])
+  private var fullscreen = false
+
+  override fun call(): Int
+  {
+    val launchProfile = LaunchProfile(fullscreen = fullscreen, soarDebugger = soarDebugger,
+        debugDrawGrid = drawZirconGrid, zirconDebugMode = zirconDebugMode)
+    L.info("Launching with profile {}", launchProfile)
+    Application(lock, condition, launchProfile)
+    lock.withLock { condition.await() }
+    L.info("Quiting...")
+    return 0
+  }
+}
+
+fun main(args: Array<String>): Unit = exitProcess(CommandLine.call(Main(), *args))
