f5538c1d1a2be752761685d326ddf52bf1264c1a
Bryan DeGrendel <bryan@degrendel.com>
2020-03-01 03:26:14 -0500

    Add more steps towards a working action queue

--- outrogue-engine/src/main/kotlin/com/degrendel/outrogue/engine/ActionQueueSystem.kt
+++ outrogue-engine/src/main/kotlin/com/degrendel/outrogue/engine/ActionQueueSystem.kt
@@ -3,23 +3,18 @@ package com.degrendel.outrogue.engine
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.EntityListener
 import com.degrendel.outrogue.common.*
-import com.degrendel.outrogue.common.ai.Action
-import com.degrendel.outrogue.common.ai.AgentController
-import com.degrendel.outrogue.common.ai.PlayerController
-import com.degrendel.outrogue.common.ai.SimpleController
+import com.degrendel.outrogue.common.ai.*
 import com.degrendel.outrogue.common.components.getCreature
 import java.util.*
 
-class ActionQueueSystem(private val world: WorldState) : EntityListener
+class ActionQueueSystem(private val engine: OutrogueEngine) : EntityListener
 {
   companion object
   {
     private val L by logger()
   }
 
-  private val queue = PriorityQueue<Entity>(10, Comparator<Entity> { e1, e2 ->
-    val c1 = e1.getCreature()
-    val c2 = e2.getCreature()
+  private val queue = PriorityQueue<CreatureState>(10, Comparator<CreatureState> { c1, c2 ->
     assert(c1 != c2)
     assert(c1.id != c2.id)
     if (c1.cooldown == c2.cooldown)
@@ -31,24 +26,25 @@ class ActionQueueSystem(private val world: WorldState) : EntityListener
   override fun entityAdded(entity: Entity)
   {
     L.info("Adding entity {} to the action queue", entity)
-    queue.add(entity)
+    queue.add(entity.getCreature() as CreatureState)
   }
 
   override fun entityRemoved(entity: Entity)
   {
-    queue.remove(entity)
+    queue.remove(entity.getCreature() as CreatureState)
   }
 
   suspend fun execute(): Action
   {
-    val entity = queue.poll()
-    val creature = entity.getCreature()
-    L.debug("Executing turn for {}, {}", entity, creature)
-    val action = when (creature.controller)
+    val creature = queue.poll()
+    val controller = creature.controller
+    L.debug("Executing turn for {}", creature)
+    val action = when (controller)
     {
       is PlayerController -> TODO("Not yet implemented")
-      is AgentController -> TODO("Agent isn't supported yet")
-      is SimpleController -> TODO("Not yet implemented")
+      // TODO: Actually implement the agent
+      is AgentController -> Sleep(creature)
+      is SimpleController -> executeSimpleAI(engine, creature, controller)
     }
     // TODO: Compute cost of performing action, put entity back into queue
     // creature.cooldown += world.computeCost(action)
