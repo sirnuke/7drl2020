cb261e0cb410bb53028d39d73b50cdb26a388e3c
Bryan DeGrendel <bryan@degrendel.com>
2020-03-01 04:24:30 -0500

    Wire up placeholder player input in the ingame view
    Mostly from the prototype.  Stub/comment out a few more things that need to be done.  Configurable input bindings would be nice though.

--- outrogue-frontend/src/main/kotlin/com/degrendel/outrogue/frontend/views/InGameView.kt
+++ outrogue-frontend/src/main/kotlin/com/degrendel/outrogue/frontend/views/InGameView.kt
@@ -1,11 +1,22 @@
 package com.degrendel.outrogue.frontend.views
 
-import com.degrendel.outrogue.common.Frontend
+import com.degrendel.outrogue.common.ai.Action
+import com.degrendel.outrogue.common.ai.Move
+import com.degrendel.outrogue.common.ai.Sleep
 import com.degrendel.outrogue.common.logger
+import com.degrendel.outrogue.common.properties.Properties.Companion.P
+import com.degrendel.outrogue.common.world.EightWay
 import com.degrendel.outrogue.frontend.Application
+import com.degrendel.outrogue.frontend.events.PlayerActionInput
 import com.degrendel.outrogue.frontend.views.fragments.WorldFragment
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.channels.Channel
+import org.hexworks.cobalt.events.api.simpleSubscribeTo
 import org.hexworks.zircon.api.ColorThemes
+import org.hexworks.zircon.api.uievent.*
 import org.hexworks.zircon.api.view.base.BaseView
+import org.hexworks.zircon.internal.Zircon
 
 class InGameView(val app: Application) : BaseView(app.tileGrid)
 {
@@ -15,10 +26,73 @@ class InGameView(val app: Application) : BaseView(app.tileGrid)
   }
 
   private val world = WorldFragment(app, screen)
+  private var job: Job? = null
+
+  // While this initial channel is not listened to (new one is created before the game loop starts), create it anyway
+  // so any inputs that happen to come in before onDock completes don't trigger a lateinit error
+  private var playerActions = Channel<Action>(capacity = P.views.world.maxQueuedActions)
 
   init
   {
     screen.theme = ColorThemes.adriftInDreams()
+    // TODO: These won't fire if something else is docked, right?
+    screen.handleKeyboardEvents(KeyboardEventType.KEY_PRESSED) { event: KeyboardEvent, _: UIEventPhase ->
+      // TODO: Would be nice to have this be configurable
+      when (event.code)
+      {
+        KeyCode.LEFT, KeyCode.KEY_A, KeyCode.NUMPAD_4 ->
+        {
+          Zircon.eventBus.publish(PlayerActionInput(Move(app.engine.world.conjurer, EightWay.WEST), this))
+          Processed
+        }
+        KeyCode.RIGHT, KeyCode.KEY_D, KeyCode.NUMPAD_6 ->
+        {
+          Zircon.eventBus.publish(PlayerActionInput(Move(app.engine.world.conjurer, EightWay.EAST), this))
+          Processed
+        }
+        KeyCode.UP, KeyCode.KEY_W, KeyCode.NUMPAD_8 ->
+        {
+          Zircon.eventBus.publish(PlayerActionInput(Move(app.engine.world.conjurer, EightWay.NORTH), this))
+          Processed
+        }
+        KeyCode.DOWN, KeyCode.KEY_X, KeyCode.NUMPAD_2 ->
+        {
+          Zircon.eventBus.publish(PlayerActionInput(Move(app.engine.world.conjurer, EightWay.SOUTH), this))
+          Processed
+        }
+        KeyCode.HOME, KeyCode.KEY_Q, KeyCode.NUMPAD_7 ->
+        {
+          Zircon.eventBus.publish(PlayerActionInput(Move(app.engine.world.conjurer, EightWay.NORTH_WEST), this))
+          Processed
+        }
+        KeyCode.PAGE_UP, KeyCode.KEY_E, KeyCode.NUMPAD_9 ->
+        {
+          Zircon.eventBus.publish(PlayerActionInput(Move(app.engine.world.conjurer, EightWay.NORTH_EAST), this))
+          Processed
+        }
+        KeyCode.END, KeyCode.KEY_Z, KeyCode.NUMPAD_1 ->
+        {
+          Zircon.eventBus.publish(PlayerActionInput(Move(app.engine.world.conjurer, EightWay.SOUTH_WEST), this))
+          Processed
+        }
+        KeyCode.PAGE_DOWN, KeyCode.KEY_C, KeyCode.NUMPAD_3 ->
+        {
+          Zircon.eventBus.publish(PlayerActionInput(Move(app.engine.world.conjurer, EightWay.SOUTH_EAST), this))
+          Processed
+        }
+        KeyCode.KEY_S, KeyCode.NUMPAD_5 ->
+        {
+          Zircon.eventBus.publish(PlayerActionInput(Sleep(app.engine.world.conjurer), this))
+          Processed
+        }
+        else -> Pass
+      }
+    }
+
+    Zircon.eventBus.simpleSubscribeTo<PlayerActionInput> {
+      L.trace("Event player action {}", it.action)
+      playerActions.offer(it.action)
+    }
   }
 
   fun refreshMap()
@@ -30,10 +104,32 @@ class InGameView(val app: Application) : BaseView(app.tileGrid)
   {
     L.info("Docking InGameView")
     world.refreshMap()
+    // Recreate the channel, since closing it
+    playerActions = Channel(capacity = P.views.world.maxQueuedActions)
+    job = app.engine.runGame()
+  }
+
+  // TODO: Not clear what should be calling this, but this is how stopping the loop should work
+  suspend fun stopGameLoop()
+  {
+    // TODO: Does the channel need to be explicitly closed?
+    job?.cancelAndJoin()
+    job = null
+  }
+
+  suspend fun getPlayerInput(): Action
+  {
+    // TODO: Do we have to check whether we've been cancelled? I think .receive() should do it for us
+    // TODO: We probably need to catch the closed exception, but where and what do we do next?
+    var action: Action
+    do
+      action = playerActions.receive()
+    while (!app.engine.isValidAction(action))
+    return action
   }
 
   override fun onUndock()
   {
-    // TODO: Do we need to do anything?  Probably stop producing player input for starters
+    // TODO: Do we need to do anything here?
   }
 }
