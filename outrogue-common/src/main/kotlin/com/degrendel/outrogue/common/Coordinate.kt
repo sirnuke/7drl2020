192be768f0f10617e6ba2dbe08f4a4f965facfd3
Bryan DeGrendel <bryan@degrendel.com>
2020-02-29 21:50:25 -0500

    Add move and validity helpers to coordinate

--- outrogue-common/src/main/kotlin/com/degrendel/outrogue/common/Coordinate.kt
+++ outrogue-common/src/main/kotlin/com/degrendel/outrogue/common/Coordinate.kt
@@ -1,3 +1,21 @@
 package com.degrendel.outrogue.common
 
+import com.degrendel.outrogue.common.properties.Properties.Companion.P
+
 data class Coordinate(val x: Int, val y: Int, val floor: Int)
+{
+  fun move(direction: Cardinal) = Coordinate(x + direction.x, y + direction.y, floor)
+
+  fun move(direction: EightWay) = Coordinate(x + direction.x, y + direction.y, floor)
+
+  fun isValid(): Boolean
+  {
+    return (x >= 0
+        && x < P.map.width
+        && y >= 0
+        && y < P.map.height
+        && floor >= 0
+        && floor < P.map.floors
+        )
+  }
+}
