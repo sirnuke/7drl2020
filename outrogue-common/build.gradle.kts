e018d88bac8750284e603bec44b1f7f425d813b7
Bryan DeGrendel <bryan@degrendel.com>
2020-02-29 22:23:36 -0500

    Add plugin to produce Git version constants
    Hardcore proper development stuff in games?  Oh yes.

--- outrogue-common/build.gradle.kts
+++ outrogue-common/build.gradle.kts
@@ -3,3 +3,19 @@ dependencies {
   api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.2")
 }
+
+plugins {
+  id("com.peterabeles.gversion") version "1.7.0"
+}
+
+gversion {
+  srcDir = "src/main/kotlin/"
+  className = "GitVersion"
+  classPackage = "com.degrendel.outrogue.common"
+  language = "kotlin"
+}
+
+tasks.compileKotlin {
+  dependsOn.add(tasks.createVersionFile)
+}
+
