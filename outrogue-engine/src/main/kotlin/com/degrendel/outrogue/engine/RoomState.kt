a7c3f224d916a7895a06b0ae012d09cf0cef4ff9
Bryan DeGrendel <bryan@degrendel.com>
2020-03-01 23:23:16 -0500

    Add forEach over all coordinates of a room

--- outrogue-engine/src/main/kotlin/com/degrendel/outrogue/engine/RoomState.kt
+++ outrogue-engine/src/main/kotlin/com/degrendel/outrogue/engine/RoomState.kt
@@ -9,18 +9,19 @@ import kotlin.random.Random
 
 data class RoomState(override val id: Int, override val entity: Entity, override val topLeft: Coordinate, override val width: Int, override val height: Int, private val random: Random) : Room
 {
-  private val tiles = mutableListOf<Coordinate>()
+  private val coordinates = mutableListOf<Coordinate>()
 
   init
   {
     entity.add(CoordinateComponent(topLeft)).add(RoomComponent(this))
     (topLeft.x until topLeft.x + width).forEach { x ->
       (topLeft.y until topLeft.y + height).forEach { y ->
-        tiles.add(Coordinate(x, y, topLeft.floor))
+        coordinates.add(Coordinate(x, y, topLeft.floor))
       }
     }
   }
 
+  fun forEach(lambda: (Coordinate) -> Unit)= coordinates.forEach(lambda)
 
   override fun isWithin(x: Int, y: Int): Boolean
   {
@@ -31,5 +32,5 @@ data class RoomState(override val id: Int, override val entity: Entity, override
   }
 
   fun getRandomSquare(filter: (coordinate: Coordinate) -> Boolean) =
-    tiles.filter(filter).shuffled(random).firstOrNull()
+    coordinates.filter(filter).shuffled(random).firstOrNull()
 }
