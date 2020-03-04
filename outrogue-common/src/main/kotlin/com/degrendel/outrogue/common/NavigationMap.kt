f9951dade2f8dd099f4841e743bd92c8c49f36dc
Bryan DeGrendel <bryan@degrendel.com>
2020-03-03 21:17:06 -0500

    Add ability to set terminate search coordinates in NavigationMap
    Also default terminate and ignore to empty set.

--- outrogue-common/src/main/kotlin/com/degrendel/outrogue/common/NavigationMap.kt
+++ outrogue-common/src/main/kotlin/com/degrendel/outrogue/common/NavigationMap.kt
@@ -6,6 +6,6 @@ import com.degrendel.outrogue.common.world.Level
 
 interface NavigationMap
 {
-  fun compute(sources: Map<Coordinate, Int>, skip: Set<Coordinate>)
+  fun compute(sources: Map<Coordinate, Int>, skip: Set<Coordinate> = setOf(), terminate: Set<Coordinate> = setOf())
   fun getBestMove(coordinate: Coordinate): EightWay?
 }
--- outrogue-engine/src/main/kotlin/com/degrendel/outrogue/engine/NavigationMapImpl.kt
+++ outrogue-engine/src/main/kotlin/com/degrendel/outrogue/engine/NavigationMapImpl.kt
@@ -1,6 +1,7 @@
 package com.degrendel.outrogue.engine
 
 import com.degrendel.outrogue.common.NavigationMap
+import com.degrendel.outrogue.common.logger
 import com.degrendel.outrogue.common.world.*
 import com.degrendel.outrogue.common.world.Square.Companion.xRange
 import com.degrendel.outrogue.common.world.Square.Companion.yRange
@@ -10,11 +11,17 @@ import kotlin.random.Random
 class NavigationMapImpl(private val random: Random, private val world: World,
                         private val filterSquares: (Square) -> Boolean = { it.creature == null && !it.type.blocked }) : NavigationMap
 {
+  companion object
+  {
+    private val L by logger()
+  }
+
   private val _data: MutableList<MutableList<Int>> = xRange.map { yRange.map { Int.MAX_VALUE }.toMutableList() }.toMutableList()
   val data: List<List<Int>> get() = _data
 
-  override fun compute(sources: Map<Coordinate, Int>, skip: Set<Coordinate>)
+  override fun compute(sources: Map<Coordinate, Int>, skip: Set<Coordinate>, terminate: Set<Coordinate>)
   {
+    L.trace("Computing navigation map for sources {}, skip {}, terminate {}", sources, skip, terminate)
     Square.each { x, y -> _data[x][y] = Int.MAX_VALUE }
     sources.forEach { (coordinate, cost) -> _data[coordinate.x][coordinate.y] = cost }
     // TODO: This needs to be a priority queue ordered by the last cost
@@ -23,8 +30,9 @@ class NavigationMapImpl(private val random: Random, private val world: World,
     {
       // We functional now (kinda)
       // TODO: This assumes there is no diagonal cost.  If that changes, this will probably need to be a float
-      val cost: Int
-      getNeighbors(toCheck.removeFirst().also { cost = _data[it.x][it.y] + 1 }, skip).forEach {
+      val checking = toCheck.removeFirst().also { if (it in terminate) return }
+      val cost = _data[checking.x][checking.y] + 1
+      getNeighbors(checking, skip).forEach {
         _data[it.x][it.y] = cost
         toCheck.addLast(it)
       }
