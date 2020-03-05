858306cd300ea48b987ebbf532d836db5b7ec2f9
Bryan DeGrendel <bryan@degrendel.com>
2020-03-05 01:32:33 -0500

    Fix issue with rolling dice
    Also reorder constructor arguments to be more consistent with other classes.

--- outrogue-common/src/main/kotlin/com/degrendel/outrogue/common/world/Dice.kt
+++ outrogue-common/src/main/kotlin/com/degrendel/outrogue/common/world/Dice.kt
@@ -2,10 +2,10 @@ package com.degrendel.outrogue.common.world
 
 import kotlin.random.Random
 
-data class Dice(val rolls: Int, val sides: Int, val random: Random)
+data class Dice(val random: Random, val rolls: Int, val sides: Int)
 {
   fun roll(): Int
   {
-    return (0 until rolls).reduce { acc, _ -> random.nextInt(1, sides + 1) }
+    return (0 until rolls).reduce { acc, _ -> acc + random.nextInt(1, sides + 1) }
   }
 }
