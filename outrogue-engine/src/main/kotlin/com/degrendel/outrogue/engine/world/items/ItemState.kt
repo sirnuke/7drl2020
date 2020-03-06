1a6bf2125b8b3dd72c665bd4413240998447ed86
Bryan DeGrendel <bryan@degrendel.com>
2020-03-05 22:44:07 -0500

    Implement base item state as an abstract class
    Should cut down on code copying in items considerably.

--- outrogue-engine/src/main/kotlin/com/degrendel/outrogue/engine/world/items/ItemState.kt
+++ outrogue-engine/src/main/kotlin/com/degrendel/outrogue/engine/world/items/ItemState.kt
@@ -1,10 +1,26 @@
 package com.degrendel.outrogue.engine.world.items
 
-import com.degrendel.outrogue.common.world.items.Item
-import com.degrendel.outrogue.common.world.items.ItemLocation
+import com.badlogic.ashley.core.Entity
+import com.degrendel.outrogue.common.components.CoordinateComponent
+import com.degrendel.outrogue.common.components.ItemComponent
+import com.degrendel.outrogue.common.world.items.*
 
-interface ItemState: Item
+abstract class ItemState(final override val entity: Entity, final override val type: ItemType,
+                         private var currentlyWhere: ItemLocation, final override val weight: Int,
+                         final override val stackable: Boolean) : Item
 {
-  var currentlyWhere: ItemLocation
-  override val where get() = currentlyWhere
+  final override val where get() = currentlyWhere
+
+  protected fun updateComponents()
+  {
+    val coordinate = currentlyWhere.let {
+      when (it)
+      {
+        is OnGround -> it.coordinate
+        is InInventory -> it.creature.coordinate
+        else -> throw IllegalStateException("Unexpected item location $it")
+      }
+    }
+    entity.add(ItemComponent(this)).add(CoordinateComponent(coordinate))
+  }
 }
